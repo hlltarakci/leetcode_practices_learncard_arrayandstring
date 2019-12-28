af6b6633a33b51597b8b220892bad0f76b2e951b
Hilal Tarakci <hlltarakci@gmail.com>
2019-12-28 19:56:53 +0300

    add solution for Reverse Words in a String III

---
+++ code/reverseWordsInAStringIII.java
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/
+class Solution {
+	/*
+		clarifying questions & edge cases:
+		test: 
+		
+		algorithm:
+            iterate through string 
+                when met a character - it is start of word
+                when met a whitespace - it is end of word
+                take reverse and put in string builder
+		
+        n: string length
+		time complexity: O(n)
+		space complexity: O(1) -- excluding output
+            output uses O(n) space
+	*/
+    public String reverseWords(String s) {
+        StringBuilder reversed = new StringBuilder();
+        
+        int startOfWord = -1;
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(Character.isWhitespace(c)) {
+                if(startOfWord == -1) reversed.append(c);
+                else {
+                    reverseAndAppend(reversed, s, startOfWord, i);
+                    startOfWord = -1;
+                    reversed.append(c);
+                }
+            } else {
+                if(startOfWord == -1) startOfWord = i;
+            }
+        }
+        
+        if(startOfWord != -1 && !Character.isWhitespace(s.charAt(s.length()-1))) 
+            reverseAndAppend(reversed, s, startOfWord, s.length());
+        
+        return reversed.toString();
+    }
+    
+    private void reverseAndAppend(StringBuilder reversed, String s, int start, int end) {
+        for(int k=end-1; k>= start; k--) reversed.append(s.charAt(k));
+    }
+}
