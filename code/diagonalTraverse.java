e73851b9811a89b259253d96bbc9a8b549649bbc
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-20 23:03:49 +0300

    Update diagonalTraverse.java

--- code/diagonalTraverse.java
+++ code/diagonalTraverse.java
@@ -1,49 +1,65 @@
-// https://leetcode.com/problems/diagonal-traverse/
+// https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
 class Solution {
-	/*
-		clarifying questions & edge cases:
-		test: 
-		
-		algorithm:
-		
-		time complexity: 
-		space complexity:
-	*/
+    /*
+        r, c: matrix rows, cols
+        time: O(r c)
+        space: O(r c) -- for output
+    */
     public int[] findDiagonalOrder(int[][] matrix) {
-        if(matrix.length == 0 || matrix[0].length == 0) return new int[0];
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return new int[0];
         
-        int rowCount = matrix.length, colCount = matrix[0].length;
-        int[] result = new int[rowCount*colCount];
+        int rows = matrix.length, cols = matrix[0].length;
+        int[] result = new int[rows*cols];
         
-        int index = 0;
         boolean isUp = true;
-        for(int r=0; r < rowCount; r++) {
-            index = process(matrix, result, index, r, 0, isUp);
-            isUp = !isUp;
-        }
-        
-        for(int c=1; c < colCount; c++) {
-            index = process(matrix, result, index, rowCount-1, c, isUp);
-            isUp = !isUp;
+        int r=0, c=0;
+        for(int i=0; i<result.length;) {
+            // valid row,col
+            if(r >= 0 && r < rows && c >= 0 && c < cols) {
+                result[i++] = matrix[r][c];
+            } 
+            
+            // isUp, invalid col
+            else if(isUp && c >= cols) {
+                c = cols-1;
+                r = r+2;
+                isUp = false;
+                continue;
+            }
+            
+            // isUp, invalid row
+            else if(isUp && r < 0) {
+                r = 0;
+                isUp = false;
+                continue;
+            }
+            
+            // isDown, invalid row
+            else if(!isUp && r >= rows) {
+                r = rows-1;
+                c = c+2;
+                isUp = true;
+                continue;
+            }
+            
+            // isDown, invalid col
+            else if(!isUp && c < 0) {
+                c = 0;
+                isUp = true;
+                continue;
+            }
+            
+            
+            if(isUp) {
+                r = r-1;
+                c = c+1;
+            } else {
+                r = r+1;
+                c = c-1;
+            }
         }
         
         return result;
     }
     
-    private int process(int[][] matrix, int[] result, int index, int row, int col, boolean isUp) {
-        if(isUp) {
-            while(row >= 0 && col < matrix[0].length) {
-                result[index++] = matrix[row--][col++];
-            }
-        } else {
-            index += Math.min(row, matrix[0].length - col - 1);
-            int tempIndex = index;
-            while(row >= 0 && col < matrix[0].length) {
-                result[tempIndex--] = matrix[row--][col++];
-            }
-            index++;
-        }
-        
-        return index;
-    }
 }
