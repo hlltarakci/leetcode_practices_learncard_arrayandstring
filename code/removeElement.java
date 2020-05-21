f18744cef149d9f96093eb840c99079e5c211821
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-21 20:50:53 +0300

    Update removeElement.java

--- code/removeElement.java
+++ code/removeElement.java
@@ -1,35 +1,21 @@
-// https://leetcode.com/problems/remove-element/
+// https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1151/
+
 class Solution {
-	/*
-		clarifying questions & edge cases:
-            all elements equal to val -- return empty list
-		test: 
-		    nums = [0,1,2,2,3,0,4,2], val = 2
-		algorithm:
-            put pointers at both ends
-            move forward
-            if at val, check end pointer
-            if end pointer val, decrement it until it is not, then get end pointer value to start pointer 
-                and decrement end pointer
-		
-        n: nums length
-		time complexity: O(n)
-		space complexity: O(1)
-	*/
+    /*
+        n: array length
+        time: O(n)
+        space: O(1)
+    */
     public int removeElement(int[] nums, int val) {
-        int start = 0, end = nums.length-1;
+        int realIndex = 0;
         
-        while(start <= end) {
-            if(nums[start] != val) {
-                start++;
-                continue;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != val) {
+                if(i != realIndex) nums[realIndex] = nums[i];
+                realIndex++;
             }
-            
-            while(start <= end && nums[end] == val) end--;
-            
-            if(start < end) nums[start++] = nums[end--];
         }
         
-        return  start;
+        return realIndex;
     }
 }
