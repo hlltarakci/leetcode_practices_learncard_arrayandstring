bcc393e144b8c092936e33035d80b0049950e67f
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-21 21:37:01 +0300

    Update removeDuplicatesFromSortedArray.java

--- code/removeDuplicatesFromSortedArray.java
+++ code/removeDuplicatesFromSortedArray.java
@@ -1,29 +1,18 @@
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1173/
 class Solution {
-	/*
-		clarifying questions & edge cases:
-            empty array
-		test: 
-		
-		algorithm:
-		    iterate through keeping two pointers:
-                one is always one step forward
-                second is at adition point of modified array
-        
+    /*
         n: array length
-		time complexity: O(n)
-		space complexity: O(1)
-	*/
+        time: O(n)
+        space: O(1)
+    */
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0) return 0;
-        
-        int index = 1;
-        
-        for(int i=1; i < nums.length; i++) {
-            if(nums[i] == nums[index-1]) continue;
-            nums[index++] = nums[i];
+        int index = 0;
+        for(int i=1; i<nums.length; i++) {
+            if( nums[i] != nums[index]) {
+                nums[++index] = nums[i];
+            }
         }
-            
-        return index;
+        
+        return index+1;
     }
 }
