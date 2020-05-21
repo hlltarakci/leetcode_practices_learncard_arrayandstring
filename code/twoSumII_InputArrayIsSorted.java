80fd1e9527a3026e438ff31cbdab3077e54f0610
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-21 20:50:08 +0300

    Update twoSumII_InputArrayIsSorted.java

--- code/twoSumII_InputArrayIsSorted.java
+++ code/twoSumII_InputArrayIsSorted.java
@@ -1,29 +1,20 @@
-// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1153/
+
 class Solution {
-	/*
-		clarifying questions & edge cases:
-            pay attention to not return same index twice
-            pay attention to return index array in order
-		test: 
-		    numbers = [2,7,11,15], target = 9
-		algorithm:
-            iterate through:
-                each element is candidate -- look for (target - current) using binary search
-		
-        n: array length
-		time complexity: O(n^2 log n)  
-		space complexity: O(1)
-	*/
+    /*
+        n: length of array
+        time: O(n)
+        space: O(1)
+    */
     public int[] twoSum(int[] numbers, int target) {
-        for(int i=0; i<numbers.length; i++) {
-            int matchingPair = Arrays.binarySearch(numbers, target - numbers[i]);
-            if(matchingPair >= 0 && matchingPair != i) {
-                int[] pair = new int[] {i+1, matchingPair+1};
-                Arrays.sort(pair);
-                return pair;
-            }
+        if(numbers == null || numbers.length < 2) return null;
+        int left = 0, right = numbers.length-1;
+        while(left < right) {
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) return new int[] {left+1, right+1};
+            if(sum > target) right--;
+            else left++;
         }
-        
-        return new int[2];
+        return null;
     }
 }
